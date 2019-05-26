797e42ac06c84adc0b6270fe41457d525a0d233e
borysewicz <33198052+borysewicz@users.noreply.github.com>
2019-05-26 12:25:38 +0200

    Add question to TestQuestionRepository

--- app/src/main/java/com/example/forehead/model/TestQuestionRepository.kt
+++ app/src/main/java/com/example/forehead/model/TestQuestionRepository.kt
@@ -11,18 +11,62 @@ class TestQuestionRepository : QuestionRepository {
     }
 
     private fun returnFictionalQuestions(): List<Question> {
-        return listOf()
+        return listOf(
+            Question("Thor","Młot, Avengers"),
+            Question("Harry Potter","Czarodziej, książka"),
+            Question("Jon Snow","Gra o tron"),
+            Question("Kaczor Donald","Zwierzę, komiks"),
+            Question("Król Lew","Zwierzę, bajka"),
+            Question("Aragorn","Wojownik, Władca Pierścieni"),
+            Question("Fineasz","Ferb, bajka"),
+            Question("Pokahontas","Indianka, bajka"),
+            Question("Nemo","Rybka, bajka"),
+            Question("Papa smerf","Niebieski z czerwoną czapką, bajka")
+        )
     }
 
     private fun returnMusicQuestions(): List<Question> {
-        return listOf()
+        return listOf(
+            Question("Karma Police", "Radiohead"),
+            Question("Bohemian Rhapsody","Queen"),
+            Question("Chciałem Być","Krzysztof Krawczyk"),
+            Question("Małomiasteczkowy","Dawid Podsiadło"),
+            Question("Wsiąść do pociągu","Maryla Rodowicz"),
+            Question("Mamma Mia", "Abba"),
+            Question("Losing my Religion","R.E.M"),
+            Question("Africa","Toto"),
+            Question("ściernisko","Golec uOrkiestra"),
+            Question("Oj ne ne","Akcent")
+        )
     }
 
     private fun returnGeographyQuestions(): List<Question> {
-        return listOf()
+        return listOf(
+            Question("Delhi","Indie"),
+            Question("Mediolan","Włochy"),
+            Question("Morena czołowa","Lodowiec"),
+            Question("Bazalt","Wulkan"),
+            Question("K-2","Himalaje"),
+            Question("Kolorado","Kanion"),
+            Question("Wyspy Wielkanocne","Posągi"),
+            Question("Morze Adriatyckie","Chorwacja"),
+            Question("Sewilla","Hiszpania"),
+            Question("Florencja","Włochy")
+        )
     }
 
     private fun returnFoodQuestions() : List<Question> {
-        return listOf()
+        return listOf(
+            Question("Awokado","Owoc"),
+            Question("Produkty zbożowe","Chleb, Mąka"),
+            Question("Sałatka jarzynowa","święta"),
+            Question("Grochówka","Wojsko"),
+            Question("Mirabelka","Kwaśna, żółta i mała"),
+            Question("Tuńczyk","Duża ryba, kot"),
+            Question("Kawior","Ikra"),
+            Question("Nutella","Słoik, czekolada"),
+            Question("Gordon Ramsay","Kucharz, Szkocja"),
+            Question("Magda Gessler","Kuchenne Rewolucje")
+        )
     }
 }
